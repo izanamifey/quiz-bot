a402fcb5a4f8801b823278355b822cec8f8d2aa8
izanamifey <tempankasw@gmail.com>
2025-09-16 01:17:13 +0700

    Create quiz-data.py

---
+++ quiz-data.py
@@ -0,0 +1,17 @@
+QUIZ_QUESTIONS = [
+    {
+        "question": "Apa ibukota Jepang?",
+        "options": ["Tokyo", "Seoul", "Beijing", "Bangkok"],
+        "answer": "Tokyo"
+    },
+    {
+        "question": "Siapa pencipta bahasa pemrograman Python?",
+        "options": ["Linus Torvalds", "Guido van Rossum", "James Gosling", "Dennis Ritchie"],
+        "answer": "Guido van Rossum"
+    },
+    {
+        "question": "2 + 2 = ?",
+        "options": ["3", "4", "5", "6"],
+        "answer": "4"
+    }
+]
